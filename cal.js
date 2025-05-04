b03bd5595a20b2be0a258fb4c32cb06bff437d19
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-05-04 15:34:28 +0500

    complete!

--- cal.js
+++ cal.js
@@ -3,43 +3,50 @@ let input = document.querySelector('#inputcal');
 
 let calculatorTr = document.querySelectorAll('tr');
 
-const arrayfrom = Array.from(calculatorTr);
-
 function calc(x, y, ope) {
-    if (ope === "+") {
-     return x + y;
-    }
-  
-    if (ope === "-") {
-        return x - y;
-    }
-  
-    if (ope === "x") {
-        return x * y;
-    }
-  
-    if (ope === "/") {
-        return x / y;
-    }
-  }
-arrayfrom.forEach(list => {
+    if (ope === "+") return x + y;
+    if (ope === "-") return x - y;
+
+    if (ope === "x") return x * y;
+
+    if (ope === "/") return x / y;
+
+    if (ope === "%") {
+        if(x > y){
+            return x % y;
+        }else{
+            return y % x;
+        }
+    };
+}
+calculatorTr.forEach(list => {
 
     list.addEventListener('click', (e) => {
-
         const clickedRow = e.target.innerText;
-        input.value += clickedRow;
 
-        if(clickedRow === '='){
-        
-         const match = input.value.match(/x/g)
-        if(match){
-            const x = Number(match[1]);
-            const ope = match[2];
-            const y = Number(match[3]);
-
-            const result = calc(x , y , ope)
-            input.value = result;
+        if(clickedRow !== "DEL" && clickedRow !== "="){
+            input.value +=  clickedRow;
         }
+
+        if (clickedRow === '=') {
+
+            const matched = input.value.match(/(\d+)([\-+x%\/])(\d+)/);
+
+            if (matched) {
+                const x = Number(matched[1]);
+                const ope = matched[2];
+                const y = Number(matched[3]);
+                const result = calc(x, y, ope)
+                input.value = result;
+            }else{
+                input.value = "Math error";
+            }
+        }
+        if (clickedRow === 'AC'){
+            input.value = "";
+        };
+        if(clickedRow === "DEL"){
+            input.value =  input.value.slice(0 ,-1);
         }
     });
 });
